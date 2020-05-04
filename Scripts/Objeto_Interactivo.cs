945e89e4e3b47371e2a353f19ea57b8083550d26
VicenteMurguiSanchis <64801404+VicenteMurguiSanchis@users.noreply.github.com>
2020-05-04 20:47:03 +0200

    Scripts realizados hasta la fecha

---
+++ Scripts/Objeto_Interactivo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Objeto_Interactivo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public bool seleccionada;
+    public string[] contenido_texto_primario;
+    public string[] contenido_texto_secundario;
+    //private int num_text_close = 0;
+    //private int num_text_open = 0;
+    //public bool cerrada;
+    public string password = "llave";
+
+    void Start()
+    {
+        //cerrada = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
